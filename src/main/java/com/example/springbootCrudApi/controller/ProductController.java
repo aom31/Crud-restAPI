6303f6bbe86559c2379e0429a55c4676f09db8dc
aom31 <97218117+aom31@users.noreply.github.com>
2022-06-16 03:50:45 +0700

    mapping in controller

---
+++ src/main/java/com/example/springbootCrudApi/controller/ProductController.java
@@ -0,0 +1,51 @@
+package com.example.springbootCrudApi.controller;
+
+import com.example.springbootCrudApi.entity.Product;
+import com.example.springbootCrudApi.service.ProductService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.PrimitiveIterator;
+
+@RestController
+public class ProductController {
+
+    @Autowired
+    private ProductService service;
+
+    @PostMapping("/addproduct")
+    public Product addProduct(@RequestBody Product product){
+        return service.saveProduct(product);
+    }
+
+    @PostMapping("/addproducts")
+    public List<Product> addProducts(@RequestBody  List<Product> products){
+        return service.saveProducts(products);
+    }
+
+    @GetMapping("products")
+    public List<Product> findAllProducts(){
+        return service.getProducts();
+    }
+
+    @GetMapping("/product/{id}")
+    public Product findProductById(@PathVariable int id){
+        return service.getProductById(id);
+    }
+
+    @GetMapping("/product/{name}")
+    public Product findProductByName(@PathVariable String name){
+        return service.getProductByName(name);
+    }
+
+    @PutMapping("/update")
+    public Product updateProduct(@RequestBody Product product){
+        return  service.updateProduct(product);
+    }
+
+    @DeleteMapping("/delete/{id}")
+    public String deleteProduct(@PathVariable int id){
+        return  service.deleteProduct(id);
+    }
+}
